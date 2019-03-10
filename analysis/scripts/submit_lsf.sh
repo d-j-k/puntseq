ce86551d4a37670ccb45eb556a7b8fbc2626612d
Michael Hall <mbhall88@gmail.com>
2019-03-10 17:04:47 +0000

    update submission script

--- analysis/scripts/submit_lsf.sh
+++ analysis/scripts/submit_lsf.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-CLUSTER_CMD=("bsub -n {cluster.nCPUs} -R {cluster.resources} -M {cluster.memory} -o {cluster.output} -e {cluster.error} -J {cluster.name} -q {cluster.queue}")
+CLUSTER_CMD=("bsub -n {threads} -R \"select[mem>{resources.mem_mb}] rusage[mem={resources.mem_mb}] span[hosts=1]\" -M {resources.mem_mb} -o {cluster.output} -e {cluster.error} -J {cluster.name} -q {cluster.queue}")
 JOB_NAME="$1"
+LOG_DIR="logs/"
 
 bsub -R "select[mem>1000] rusage[mem=1000]" \
-  -M 1000 \
-  -o logs/cluster_"$JOB_NAME".o \
-  -e logs/cluster_"$JOB_NAME".e \
-  -J "$JOB_NAME" \
-  -q research-rh74 \
-  snakemake --use-singularity \
+    -M 1000 \
+    -o "$LOG_DIR"/cluster_"$JOB_NAME".o \
+    -e "$LOG_DIR"/cluster_"$JOB_NAME".e \
+    -J "$JOB_NAME" \
+    -q research-rh74 \
+    snakemake --use-singularity \
     --cluster-config cluster.yaml \
-    --jobs 500 \
-    --cluster "${CLUSTER_CMD[@]}" \
-    --restart-times 3
+    --jobs 2000 \
+    --restart-times 3 \
+    --cluster "${CLUSTER_CMD[@]}"
 
 exit 0
